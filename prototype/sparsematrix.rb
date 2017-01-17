d847ea217cd4e689558c7c6bc059f4dca80d7242
vitor-vfm <vitor.vfm@gmail.com>
2017-01-17 15:18:17 -0700

    Start prototype

---
+++ prototype/sparsematrix.rb
@@ -0,0 +1,17 @@
+
+class SparseMatrix
+    def transpose()    
+    end
+
+    def inverse()
+    end 
+
+    def rank()
+    end
+
+    def determinant()
+    end
+
+    def addToScalar()
+    end
+end
