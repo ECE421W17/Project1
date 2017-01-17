4455fa501a5aef06a25e94f50d58104b2172cbbe
vitor-vfm <vitor.vfm@gmail.com>
2017-01-17 16:22:03 -0700

    Start factory and tridiagonal matrix

---
+++ prototype/tridiagonalmatrix.rb
@@ -0,0 +1,11 @@
+class TriDiagonalMatrix
+    def initialize()
+        @internal_rep = Array.new
+    end
+
+    def initialize(*arrays)
+        initialize()
+        # TODO: implement
+    end
+
+end
