dda024cefc855105315934e5f275b7e577a2485a
vitor-vfm <vitor.vfm@gmail.com>
2017-02-04 14:00:26 -0700

    Create a SparseMatrix module (abstract class)

---
+++ sparse_matrix.rb
@@ -0,0 +1,65 @@
+module SparseMatrix
+
+    def hashRepresentation()
+        return @internal_rep
+    end
+
+    def +(other)
+        if other.is_a(SparseMatrix)
+            _addToMatrix(other)
+        elsif other.is_a(Numeric)
+            _addToScalar(other)
+        end
+    end
+
+    def -(other)
+        if other.is_a(SparseMatrix)
+            _subtractMatrix(other)
+        elsif other.is_a(Numeric)
+            _addToScalar(-other)
+        end
+    end
+
+    def *(other)
+        if other.is_a(SparseMatrix)
+            _multiplyByMatrix(other)
+        elsif other.is_a(Numeric)
+            _multiplyByScalar(other)
+        end
+    end
+
+    def get(*indices)
+        @internal_rep[indices]
+    end
+
+    def set(new_value, *indices)
+        @internal_rep[indices] = new_value
+    end
+
+    def transpose()
+    end
+
+    def inverse()
+    end
+
+    def rank()
+    end
+
+    def determinant()
+    end
+
+    def _addToScalar(other)
+    end
+
+    def _addToMatrix(other)
+    end
+
+    def _multiplyByMatrix(other)
+    end
+
+    def _multiplyByScalar(other)
+    end
+
+    def _subtractMatrix(other)
+    end
+end
