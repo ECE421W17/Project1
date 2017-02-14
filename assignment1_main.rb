a91ac0e0dbcce9891bc5035de51a0e5bd50e5f13
vitor-vfm <vitor.vfm@gmail.com>
2017-02-14 13:54:15 -0700

    Add the main file

---
+++ assignment1_main.rb
@@ -0,0 +1,6 @@
+require_relative 'sparse_matrix'
+require_relative 'abstract_matrix_factory'
+require_relative 'sparse_matrix_factory'
+require_relative 'matrix_factory'
+require_relative 'dok_matrix'
+
