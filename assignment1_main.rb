110ffb31e87e6a12baeecf41535e149ee45e775f
vitor-vfm <vitor.vfm@gmail.com>
2017-02-14 14:06:31 -0700

    Names and comments

--- assignment1_main.rb
+++ assignment1_main.rb
@@ -1,6 +1,16 @@
-require_relative 'sparse_matrix'
-require_relative 'abstract_matrix_factory'
-require_relative 'sparse_matrix_factory'
-require_relative 'matrix_factory'
-require_relative 'dok_matrix'
+require './sparse_matrix'
+require './abstract_matrix_factory'
+require './sparse_matrix_factory'
+require './matrix_factory'
+require './dok_matrix'
 
+# Students
+# Rui "James" Wu
+# Derek Shultz
+# Andrew Lawrence
+# Vitor Mendonca
+
+# To run the program, open a ruby console such as irb
+# and require this file
+#
+# The main class to use is DoKMatrix. Refer to documentation for use
