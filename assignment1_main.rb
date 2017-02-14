c37ac6eef67e923eea92e03aaefc521b79b8e856
Derek Shultz <shultz.derek@gmail.com>
2017-02-14 14:23:18 -0700

    Added my tests to main file

--- assignment1_main.rb
+++ assignment1_main.rb
@@ -58,4 +58,49 @@ other_res_mat.iterate { |row, col, val|
   puts 'Next val: ' + val.to_s
 }
 
+myMatrix = DoKMatrix.new(3, 2)
+myMatrix.set(1, 1, 0)
+myMatrix.to_s
+
+transposed = DoKMatrix.new(2, 3)
+transposed = myMatrix.transpose
+transposed.to_s
+
+myMatrix = myMatrix._multiplyByScalar(2)
+myMatrix.to_s
+
+newMatrix = myMatrix._multiplyByDoKMatrix(transposed)
+newMatrix.to_s
+
+myMatrix = myMatrix*2
+myMatrix.to_s
+
+myMatrix = myMatrix*transposed
+myMatrix.to_s
+
+myOther = DoKMatrix.new(3, 3)
+myOther.set(1, 0, 0)
+myOther.set(1, 1, 1)
+myOther.set(1, 1, 2)
+myOther.set(1, 1, 0)
+myOther.set(1, 2, 2)
+myOther.inverse.to_s
+myOtherInverse = myOther.inverse
+
+(myOther / myOtherInverse).to_s
+
+myMatrix = myMatrix / 2
+myMatrix.to_s
+
+aMatrix = Matrix.build(3, 2) {|row, col| 1}
+mult = myMatrix * aMatrix
+mult.to_s
+
+
+aMatrix = Matrix.build(3, 3) {|row, col| 1}
+aMatrix.send(:[]=, 0, 2, 0)
+aMatrix.send(:[]=, 2, 0, 0)
+mult = myMatrix / aMatrix
+mult.to_s
+
 # *****
